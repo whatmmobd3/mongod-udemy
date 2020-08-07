bda6dba5ee6a9351a02e3735aa789698aff53d81
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-08-07 09:46:28 +0700

    schema

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connect mongoose"))
   .catch((err) => console.log("Error ", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  data: { type: Data, default: Data.now },
+  isPublished: Boolean,
+});
